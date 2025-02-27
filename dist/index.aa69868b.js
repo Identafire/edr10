7e8484a86557430c077d77f181b4a57609e9a65a
Aaron Joseph <aaron@identafire.com>
2025-02-26 16:40:31 -0800

    stg v1.4
    stat boxes fixed; all links in new window; timeline fixes

--- dist/index.aa69868b.js
+++ dist/index.aa69868b.js
@@ -17,19 +17,7 @@ handleMediaQueryChange(mediaQuery);
 function clickToOpen() {
     $('#mainNav').toggleClass('close');
     $('#mainNav').toggleClass('open');
-} // // import 'bootstrap/js/dist/alert';
- // import 'bootstrap/js/dist/button';
- // import 'bootstrap/js/dist/carousel';
- // import 'bootstrap/js/dist/collapse';
- // // import 'bootstrap/js/dist/dropdown';
- // // import 'bootstrap/js/dist/modal';
- // import 'bootstrap/js/dist/offcanvas';
- // // import 'bootstrap/js/dist/popover';
- // import 'bootstrap/js/dist/scrollspy';
- // // import 'bootstrap/js/dist/tab';
- // // import 'bootstrap/js/dist/toast';
- // import 'bootstrap/js/dist/tooltip';
- // define an observer instance
+} // define an observer instance
  // const numSteps = 20.0;
  // let boxElement;
  // let prevRatio = 0.0;
--- src/js/main.js
+++ src/js/main.js
@@ -25,20 +25,6 @@ function clickToOpen() {
   $('#mainNav').toggleClass('open');
 }
 
-
-
-// // import 'bootstrap/js/dist/alert';
-// import 'bootstrap/js/dist/button';
-// import 'bootstrap/js/dist/carousel';
-// import 'bootstrap/js/dist/collapse';
-// // import 'bootstrap/js/dist/dropdown';
-// // import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/offcanvas';
-// // import 'bootstrap/js/dist/popover';
-// import 'bootstrap/js/dist/scrollspy';
-// // import 'bootstrap/js/dist/tab';
-// // import 'bootstrap/js/dist/toast';
-// import 'bootstrap/js/dist/tooltip';
 // define an observer instance
 // const numSteps = 20.0;
 
